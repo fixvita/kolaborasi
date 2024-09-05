685c5a719ae728699e4df284dd7c0ecae2fb860e
Novita Anggraini <71119823+fixvita@users.noreply.github.com>
2024-09-05 16:33:52 +0700

    Create kolab.py

---
+++ kolab.py
@@ -0,0 +1,15 @@
+def luas_segitiga():
+    a = int(input("masukkan alas segitiga: "))
+    t = int(input("masukkan tinggi segitiga: "))
+    luas = a*t/2
+    
+    
+    print("luas segitiga adalah: ", luas)
+    
+luas_segitiga()
+
+def luas_persegi():
+    a = int(input("panjang sisi persegi"))
+    luas = a*a
+    print("luas pesegi ",luas_persegi)
+luas_persegi()
